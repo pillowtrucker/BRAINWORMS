f6fe1aee211202ca47d9784b90bccbf31fbd6a93
pillowtrucker <githubino@janitor.chat>
2023-12-21 15:27:18 +0100

    WIP start thinking about the framework I need/want

--- src/backstage/plumbing.rs
+++ src/backstage/plumbing.rs
@@ -1,2 +1,4 @@
 pub(crate) mod frame_rate;
 pub(crate) mod platform_scancodes;
+#[cfg(target_arch = "wasm32")]
+pub(crate) mod resize_observer;
--- src/play.rs
+++ src/play.rs
@@ -1,1 +1,7 @@
+pub mod stage2d;
 pub mod stage3d;
+#[cfg(target_arch = "wasm32")]
+use crate::backstage::plumbing::resize_observer;
+pub use parking_lot::{Mutex, MutexGuard};
+/// I definitely want the minimal asset loader and the grabber from r3f
+pub use rend3_framework::{AssetError, AssetLoader, AssetPath, Grabber, UserResizeEvent};
